a12c2d6fd4cea00114e70b9eb89f1b0b5eea21e2
Duncan Levings <45014593+DuncanLevings@users.noreply.github.com>
2019-01-25 21:43:22 -0500

    Create ActiveSkillbitBase.cs

---
+++ CS/School/InnerHero/Skills/ActiveSkillbitBase.cs
@@ -0,0 +1,61 @@
+// Decompiled with JetBrains decompiler
+// Type: ActiveSkillbitBase
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: FF427592-9464-4031-9AF7-E8EEDF896718
+// Assembly location: C:\Users\Duncan\Desktop\Assembly-CSharp.dll
+
+using UnityEngine;
+
+public abstract class ActiveSkillbitBase : MonoBehaviour
+{
+  public bool Active;
+  public float UseCooldown;
+  [HideInInspector]
+  public GameObject target;
+  [HideInInspector]
+  public GameObject playerObj;
+  protected Animator anim;
+  protected SearchForObjectSB[] searchSBList;
+
+  protected ActiveSkillbitBase()
+  {
+    base.\u002Ector();
+  }
+
+  public abstract void getEventTarget();
+
+  public abstract void UseSkillbit();
+
+  protected void consumeEvent()
+  {
+    if (!Object.op_Implicit((Object) this.target.GetComponent<DoorType>()))
+      TimelineBar.Instance.SetMoveElement();
+    string name = ((Object) this.target).get_name();
+    Timeline.instance.TimeLineHistory.Add(name);
+    Object.Destroy((Object) this.target);
+    this.target = (GameObject) null;
+    Timeline.instance.RemoveFirstEvent();
+  }
+
+  protected void consumeEventNoDestroy()
+  {
+    if (!Object.op_Implicit((Object) this.target.GetComponent<DoorType>()))
+      TimelineBar.Instance.SetMoveElement();
+    string name = ((Object) this.target).get_name();
+    Timeline.instance.TimeLineHistory.Add(name);
+    if (!Timeline.instance.TimeLineEmptyCheck() && Timeline.instance.TimeLineEvents[0].eventsInRoom.Contains(this.target) && Object.op_Equality((Object) Timeline.instance.FirstEvent(), (Object) this.target))
+      Timeline.instance.RemoveFirstEvent();
+    ((IsAEntityType) this.target.GetComponent<IsAEntityType>()).type = IsAEntityType.Type.Undefined;
+    this.target = (GameObject) null;
+  }
+
+  protected SearchForObjectSB getEventType(SearchForObjectSB.SearchType type)
+  {
+    for (int index = 0; index < this.searchSBList.Length; ++index)
+    {
+      if (this.searchSBList[index].type == type)
+        return this.searchSBList[index];
+    }
+    return (SearchForObjectSB) null;
+  }
+}
