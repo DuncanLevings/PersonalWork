922a552e3f9ded458e81cbc69303378a471b28ea
Duncan Levings <45014593+DuncanLevings@users.noreply.github.com>
2019-04-18 08:16:38 -0400

    Create index.js

---
+++ Web Development/School/FindASpot/js/index.js
@@ -0,0 +1,35 @@
+/*
+Author: Duncan Levings
+ */
+
+var controller;
+var app = {
+    // Application Constructor
+    initialize: function() {
+        if (navigator.userAgent.match(/(Android)/)) {
+            document.addEventListener("deviceready", this.onDeviceReady, false);
+        } else {
+            this.onDeviceReady();
+        }
+    },
+
+    onDeviceReady: function() {
+        controller = new Controller();
+        app.overrideBrowserAlert();
+    },
+
+    overrideBrowserAlert: function() {
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "FindParking", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }
+};
+
+app.initialize();
